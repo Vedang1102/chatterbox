883258b2af1b4fa6c1b3b022d8ebae138668ed8f
Vedang1102 <atgurvedang@gmail.com>
2025-01-16 16:55:39 +0530

    added db

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = (req,res) => {
+    
+}
+
+export const login = (req,res) => {
+    res.send("Login Page");
+}
+
+export const logout = (req,res) => {
+    res.send("Logout Page");
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,15 @@
 import express from "express";
+import authRoutes from "../src/routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
-
+dotenv.config()
 const app = express();
+const PORT = process.env.PORT;
 
-app.listen(5001, () => {
-    console.log("Server is running on port 5001");
+app.use("/api/auth", authRoutes)
+
+app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+    connectDB();
 });
