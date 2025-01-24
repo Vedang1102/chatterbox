818e59a13794afc55e8e823969e8aa1a96b95511
Vedang1102 <atgurvedang@gmail.com>
2025-01-24 17:16:14 +0530

    Deployment remaining

--- server/src/index.js
+++ server/src/index.js
@@ -5,9 +5,10 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
+import { app, server} from "./lib/socket.js";
 
-dotenv.config()
-const app = express();
+dotenv.config();
+
 const PORT = process.env.PORT;
 
 app.use(express.json());
@@ -22,7 +23,7 @@ app.use(cors(
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on port " + PORT);
     connectDB();
 });
